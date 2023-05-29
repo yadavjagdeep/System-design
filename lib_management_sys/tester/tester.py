f1cbb84512ace96edb84aebfa3ab5b682300a9ca
yadavjagdeep <7jdeep@gmail.com>
2023-05-30 00:22:16 +0530

    lld library management system

--- lib_management_sys/tester/tester.py
+++ lib_management_sys/tester/tester.py
@@ -1,4 +1,4 @@
-from auth.user_authanticator import UserAuthenticator
+from lib_management_sys.auth.user_authanticator import UserAuthenticator
 
 class Tester:
 
@@ -6,7 +6,7 @@ class Tester:
         if not book_name:
             raise AttributeError("Book name cannot be Null !!!")
 
-        from searcher.search_by_book_name import SearchByBookName
+        from lib_management_sys.searcher.search_by_book_name import SearchByBookName
         BookSearcher = SearchByBookName(book_name)
         return BookSearcher.search()
 
@@ -14,7 +14,7 @@ class Tester:
         if not author_name or len(author_name) == 0:
             raise AttributeError("Author name cannot be Null !!!")
 
-        from searcher.search_by_author_name import SearchByAuthorName
+        from lib_management_sys.searcher.search_by_author_name import SearchByAuthorName
         BookSearcher = SearchByAuthorName(author_name)
         return BookSearcher.search()
 
@@ -24,7 +24,7 @@ class Tester:
         if not UserAuthenticator.isAdmin(token):
             raise PermissionError("User is not authorised to perform the operation")
 
-        from searcher.search_by_member_name import SearchByMemberName
+        from lib_management_sys.searcher.search_by_member_name import SearchByMemberName
         BookSearcher = SearchByMemberName(member_name)
         return BookSearcher.search()
 
@@ -35,7 +35,7 @@ class Tester:
         if not UserAuthenticator.isAdmin(token):
             raise PermissionError("User is not authorised to perform the operation")
 
-        from searcher.search_by_member_id import SearchByMemberId
+        from lib_management_sys.searcher.search_by_member_id import SearchByMemberId
         BookSearcher = SearchByMemberId(member_id)
         return BookSearcher.search()
 
@@ -46,12 +46,12 @@ class Tester:
         """
         Validation logic goes here
         """
-        from book.book_details import BookDetails
-        from book.book_copy import BookCopy
-        from id_genrator.book_copy_id_genrator import BookCopyIdGenerator
+        from lib_management_sys.book.book_details import BookDetails
+        from lib_management_sys.book.book_copy import BookCopy
+        from lib_management_sys.id_genrator.book_copy_id_genrator import BookCopyIdGenerator
         book_copy: object = BookCopy(BookDetails(book_name=book_name, publication_date=publicationDate, author=author), book_id=BookCopyIdGenerator.getUniqueBookCopyId(book_name))
 
-        from library.library import Library
+        from lib_management_sys.library.library import Library
         Library().addBook(book_copy)
 
 
@@ -62,12 +62,12 @@ class Tester:
         if not book_copy_id:
             raise AttributeError("Book copy Id cannot be null !!!")
 
-        from searcher.search_book_by_id import SearchBookById
+        from lib_management_sys.searcher.search_book_by_id import SearchBookById
         book_copies = SearchBookById(book_copy_id).search()
         if not book_copies:
             raise RuntimeError("No book copies found for the copy_ids")
 
-        from library.library import Library
+        from lib_management_sys.library.library import Library
         for item in book_copies:
             Library().deleteBook(item)
 
@@ -79,17 +79,17 @@ class Tester:
         if not book_copy_ids or not member_id:
             raise AttributeError("Book copy ids or member ids cannot be null !!!")
 
-        from searcher.search_book_by_id import SearchBookById
+        from lib_management_sys.searcher.search_book_by_id import SearchBookById
         book_copies = SearchBookById(book_copy_ids).search()
         if not book_copies:
             raise RuntimeError("No book copy found with the given ids")
 
-        from searcher.search_by_member_id import SearchByMemberId
+        from lib_management_sys.searcher.search_by_member_id import SearchByMemberId
         member = SearchByMemberId([member_id]).search()
         if not member:
             raise RuntimeError("No member found with given member id")
 
-        from library.library import Library
+        from lib_management_sys.library.library import Library
         for book_copy in book_copies:
             Library().issueBook(book_copy, member)
 
@@ -99,17 +99,17 @@ class Tester:
         if not book_copy_ids or not member_id:
             raise AttributeError("Book copy ids or member ids cannot be null !!!")
 
-        from searcher.search_book_by_id import SearchBookById
+        from lib_management_sys.searcher.search_book_by_id import SearchBookById
         book_copies = SearchBookById(book_copy_ids).search()
         if not book_copies:
             raise RuntimeError("No book copy found with the given ids")
 
-        from searcher.search_by_member_id import SearchByMemberId
+        from lib_management_sys.searcher.search_by_member_id import SearchByMemberId
         member = SearchByMemberId([member_id]).search()
         if not member:
             raise RuntimeError("No member found with given member id")
 
-        from library.library import Library
+        from lib_management_sys.library.library import Library
         for book_copy in book_copies:
             Library().submitBook(book_copy, member)
 
@@ -121,12 +121,12 @@ class Tester:
         if not member_id:
             raise AttributeError("Member id cannot be None !!!")
 
-        from searcher.search_by_member_id import SearchByMemberId
+        from lib_management_sys.searcher.search_by_member_id import SearchByMemberId
         members = SearchByMemberId([member_id]).search()
         if not members:
             raise RuntimeError("No member found with the provided member id !!!")
 
-        from library.library import Library
+        from lib_management_sys.library.library import Library
         for member in members:
             Library().blockMember(member)
 
@@ -135,12 +135,12 @@ class Tester:
         if not member_id:
             raise AttributeError("Member id cannot be null !!!")
 
-        from searcher.search_by_member_id import SearchByMemberId
+        from lib_management_sys.searcher.search_by_member_id import SearchByMemberId
         member = SearchByMemberId([member_id]).search()
         if not member:
             raise RuntimeError("No member with the provided member id")
 
-        from library.library import Library
+        from lib_management_sys.library.library import Library
         Library().getBorrowedBooks(member)
 
     def getBorrowerOfBooks(self, book_copy_id: str, token: str) -> object:
@@ -150,12 +150,12 @@ class Tester:
         if not book_copy_id:
             raise AttributeError("Book copy id cannot be null !!!")
 
-        from searcher.search_book_by_id import SearchBookById
+        from lib_management_sys.searcher.search_book_by_id import SearchBookById
         book_copy = SearchBookById([book_copy_id]).search()
         if not book_copy:
             raise RuntimeError("No book copy with the provided book copy id")
 
-        from library.library import Library
+        from lib_management_sys.library.library import Library
         member = Library().getBorrower(book_copy)
         if not member:
             raise RuntimeError("No Borrower of the provided book copy id")
