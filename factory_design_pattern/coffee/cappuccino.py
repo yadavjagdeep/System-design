0f0c286669d8914237ec93002939566877837c18
yadavjagdeep <7jdeep@gmail.com>
2023-06-28 14:09:53 +0530

    factory

---
+++ factory_design_pattern/coffee/cappuccino.py
@@ -0,0 +1,14 @@
+from factory_design_pattern.coffee.coffee_interface import Coffee
+
+
+class Cappuccino(Coffee):
+
+    def __init__(self, CappuccinoIngredients: object):
+        super().__init__(ingredients=CappuccinoIngredients)
+
+    def brew(self):
+        print(f"brew {self.__class__.__name__}")
+
+    def boil(self):
+        print(f"boil {self.__class__.__name__}")
+
---
+++ factory_design_pattern/sugar/sugar.py
@@ -0,0 +1,5 @@
+from abc import ABC
+
+
+class Sugar(ABC):
+    pass
